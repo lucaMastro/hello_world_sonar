b193e8ee9e7f2a85d0a13be0102f5d3a97c9c79d
Luca <luca.mastrobattista@students.uniroma2.eu>
2021-03-22 13:23:16 +0100

    moved system.out in system.err

--- src/logic/HelloWorld.java
+++ src/logic/HelloWorld.java
@@ -3,6 +3,6 @@ package logic;
 public class HelloWorld {
 
     public static void main(String[] args) {
-        System.out.println("hello world");
+        System.err.println("hello world");
     }
 }
